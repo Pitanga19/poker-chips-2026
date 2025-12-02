ed577022b92bc1f33c6c08551eb226916efef97a
Pitanga <lcmaciel99@gmail.com>
2025-12-02 10:56:41 -0300

    Agregar relación con modelo Game

--- fastapi/app/db/tables/tables/model.py
+++ fastapi/app/db/tables/tables/model.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 from sqlalchemy import Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from typing import List, TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 from app.db.base_class import Base
 
 if TYPE_CHECKING:
     from app.db.tables.rooms.model import Room
     from app.db.tables.seats.model import Seat
+    from app.db.tables.games.model import Game
 
 class Table(Base):
     __tablename__ = 'tables'
@@ -24,3 +25,6 @@ class Table(Base):
         back_populates='table',
         cascade='all, delete-orphan'
     )
+    
+    # relación 1:1 opcional con Game
+    game: Mapped[Optional['Game']] = relationship('Game', back_populates='table', uselist=False)
