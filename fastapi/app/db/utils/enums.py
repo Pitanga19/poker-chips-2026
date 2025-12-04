97c8e9004dc964ccf22f91af555a1072a4c4e085
Pitanga <lcmaciel99@gmail.com>
2025-12-04 11:59:38 -0300

    Agregar acción 'must-all-in'

--- fastapi/app/db/utils/enums.py
+++ fastapi/app/db/utils/enums.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
 class ActionType(str, Enum):
-    PUT_SB = "put-sb"
-    PUT_BB = "put-bb"
-    CHECK = "check"
-    CALL = "call"
-    BET = "bet"
-    RAISE = "raise"
-    ALL_IN = "all-in"
-    FOLD = "fold"
+    PUT_SB = 'put-sb'
+    PUT_BB = 'put-bb'
+    CHECK = 'check'
+    CALL = 'call'
+    BET = 'bet'
+    RAISE = 'raise'
+    ALL_IN = 'all-in'
+    MUST_ALL_IN = 'must-all-in'
+    FOLD = 'fold'
