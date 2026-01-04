442e1daf6c7ddb84c9ace2e3b7e6a2c47ba4e92f
Pitanga <lcmaciel99@gmail.com>
2026-01-04 06:41:48 -0300

    Crear mapper para convertir entre estados dataclass y esquemas pydantic

---
+++ fastapi/app/modules/in_game/api/mapper.py
@@ -0,0 +1,158 @@
+from typing import List
+from uuid import UUID
+from app.db.tables.hands.schemas import HandStreet
+from app.modules.in_game.engine.managers.action_manager import ActionDescriptor
+from app.modules.in_game.engine.managers.showdown_manager import (
+    ShowdownPotInfo,
+    ShowdownPotWinners,
+    PayoutDescription,
+)
+from app.modules.in_game.api.schemas import (
+    InGamePlayerInfo,
+    GameStartRequest,
+    GameStartResponse,
+    ActionDescriptorView,
+    AvailableActionsResponse,
+    ShowdownPotInfoView,
+    ShowdownInfoResponse,
+    PayoutDescriptionView,
+    ShowdownResolveRequest,
+    ShowdownResolveResponse,
+)
+from app.modules.in_game.engine.game_states import (
+    PlayerState,
+    PotState,
+    BetRoundState,
+    HandState,
+    GameState,
+)
+
+class GameMapper:
+    @staticmethod
+    def request_to_game_state(request: GameStartRequest, game_id: UUID) -> GameState:
+        players = [
+            PlayerState(
+                id=player.id,
+                username=player.username,
+                position=i,
+                stack=player.stack,
+            )
+            for i, player in enumerate(request.players)
+        ]
+        
+        main_pot = PotState(
+            size=0,
+            players_in_pot=[p.id for p in players],
+            pot_winners=[],
+        )
+        
+        hand = HandState(
+            street=HandStreet.PRE_FLOP,
+            small_blind_value=request.small_blind_value,
+            big_blind_value=request.big_blind_value,
+            can_act_positions=[p.position for p in players],
+            dealer_position=None,
+        )
+        
+        bet_round = BetRoundState()
+        
+        return GameState(
+            id=game_id,
+            players=players,
+            pots=[main_pot],
+            hand=hand,
+            bet_round=bet_round,
+        )
+    
+    @staticmethod
+    def game_state_to_start_response(gs: GameState) -> GameStartResponse:
+        # Implementación para mapear GameState a GameStartResponse
+        dealer = gs.players_by_position[gs.hand.dealer_position]
+        small_blind = gs.players_by_position[gs.hand.small_blind_position]
+        big_blind = gs.players_by_position[gs.hand.big_blind_position]
+        current_player = gs.current_player
+        
+        return GameStartResponse(
+            game_id=gs.id,
+            street=gs.hand.street,
+            dealer_id=dealer.id,
+            small_blind_id=small_blind.id,
+            big_blind_id=big_blind.id,
+            current_player_id=current_player.id,
+            players=[
+                InGamePlayerInfo(
+                    id=p.id,
+                    username=p.username,
+                    position=p.position,
+                    stack=p.stack,
+                    betting_stack=p.betting_stack,
+                )
+                for p in gs.players
+            ],
+        )
+    
+    @staticmethod
+    def request_to_available_actions_response(
+        player: PlayerState,
+        actions: List[ActionDescriptor],
+    ) -> AvailableActionsResponse:
+        player_info = InGamePlayerInfo(
+                id=player.id,
+                username=player.username,
+                position=player.position,
+                stack=player.stack,
+                betting_stack=player.betting_stack,
+            )
+        
+        actions_views = [
+            ActionDescriptorView(
+                type=action.type,
+                min_amount=action.min_amount,
+                max_amount=action.max_amount,
+            )
+            for action in actions
+        ]
+        
+        return AvailableActionsResponse(
+            player=player_info,
+            actions=actions_views,
+        )
+    
+    @staticmethod
+    def showdown_pots_info_to_response(pots_info: List[ShowdownPotInfo]) -> ShowdownInfoResponse:
+        pots_to_resolve = [
+            ShowdownPotInfoView(
+                pot_index=pot_info.pot_index,
+                pot_size=pot_info.pot_size,
+                players_in_pot=pot_info.players_in_pot,
+            )
+            for pot_info in pots_info
+        ]
+        
+        return ShowdownInfoResponse(pots_to_resolve=pots_to_resolve)
+    
+    @staticmethod
+    def request_to_showdown_pots_winners(
+        request: ShowdownResolveRequest,
+    ) -> List[ShowdownPotWinners]:
+        pots_winners = [
+            ShowdownPotWinners(
+                pot_index=pot_winner.pot_index,
+                pot_winners_ids=pot_winner.pot_winners_ids,
+            )
+            for pot_winner in request.pots_winners
+        ]
+        
+        return pots_winners
+    
+    @staticmethod
+    def payouts_to_showdown_resolve_response(payouts: List[PayoutDescription]) -> ShowdownResolveResponse:
+        payouts_descriptions = [
+            PayoutDescriptionView(
+                player_id=payout.player_id,
+                amount_won=payout.amount_won,
+            )
+            for payout in payouts
+        ]
+        
+        return ShowdownResolveResponse(payout_descriptions=payouts_descriptions)
