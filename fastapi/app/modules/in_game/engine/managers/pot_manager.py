d5cf4931fed31c21e51aa124a7eda6dbf405311a
Pitanga <lcmaciel99@gmail.com>
2026-01-08 08:39:12 -0300

    Corregir main pot inicial, modificar manejo de side pots

--- fastapi/app/modules/in_game/engine/managers/pot_manager.py
+++ fastapi/app/modules/in_game/engine/managers/pot_manager.py
@@ -2,11 +2,10 @@ from dataclasses import dataclass
 from random import randint
 from typing import List
 from app.core.exceptions import ValidationException
-from app.db.utils.enums import ActionType
 from app.modules.in_game.engine.game_states import GameState
 from app.modules.in_game.engine.utils.pot_utils import (
     reset_pot_list,
-    add_can_act_players_to_pot,
+    add_active_players_to_pot,
     return_bet_chips,
     collect_to_pot,
     add_pot_to_list,
@@ -32,7 +31,7 @@ class PotManager:
         - Agrega los jugadores que pueden jugar al main pot
         """
         reset_pot_list(game_state.pots)
-        add_can_act_players_to_pot(game_state.pots[0], game_state.players)
+        add_active_players_to_pot(game_state.pots[0], game_state.players)
     
     @staticmethod
     def collect_bets_into_pots(game_state: GameState) -> None:
@@ -48,31 +47,44 @@ class PotManager:
         # Recolectar apuestas de jugadores foldeados antes de crear side-pots
         folded_with_betting_stack = [
             p for p in players
-            if p.last_action == ActionType.FOLD and p.betting_stack > 0
+            if not p.is_active and p.betting_stack > 0
         ]
         collect_to_pot(folded_with_betting_stack, pots[-1])
         
+        # Obtener IDs de jugadores involucrados en el pozo actual
+        players_in_pot = game_state.players_in_last_pot
+        
+        # Obtener jugadores con apuestas
         betting_players = [p for p in players if p.betting_stack > 0]
         betting_players.sort(key=lambda p: p.betting_stack)
         
+        # Detener si no hay jugadores apostando
+        if not betting_players:
+            return
+        
+        # Verificar side-pot por jugadores all-in
+        if len(betting_players) < len(players_in_pot):
+            add_pot_to_list(pots, betting_players)
+        
         while len(betting_players) > 1:
-            # Todos apostaron lo mismo
+            # Si todos apostaron lo mismo recolectar y finalizar
             if betting_players[0].betting_stack == betting_players[-1].betting_stack:
                 # pots[-1] es el pot activo
                 collect_to_pot(betting_players, pots[-1])
                 break
             
-            # Side-pot
+            # Si se necesita side-pot recolectar el mínimo apostado al pot actual
             min_amount = betting_players[0].betting_stack
             
-            betting_players = collect_to_pot(
-                betting_players,
-                pots[-1],
-                min_amount
-            )
+            # Recolecta el mínimo
+            collect_to_pot(betting_players, pots[-1], min_amount)
             
+            # Quitar jugadores que ya no tengan fichas apostadas
+            betting_players = [p for p in betting_players if p.betting_stack > 0]
+            
+            # Si queda más de un jugador con fichas apostadas crea un nuevo pot
             if len(betting_players) > 1:
-                add_pot_to_list(pots, betting_players)  # muta la lista
+                add_pot_to_list(pots, betting_players)
         
         # Devolver betting_stack si queda un solo jugador
         if len(betting_players) == 1:
