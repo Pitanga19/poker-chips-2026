ca51ff8c81959c5cf46b997fa9a1c89f52300bb4
Pitanga <lcmaciel99@gmail.com>
2026-01-13 11:43:36 -0300

    Crear elementos para formularios

---
+++ native/src/ui/Forms/Form.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native'
+
+export const Form = styled.View`
+  flex: 1;
+  gap: ${({ theme }) => theme.spacing.md}px;
+`
---
+++ native/src/ui/Forms/FormSubmit.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native'
+
+export const SubmitContainer = styled.View`
+  margin-top: auto;
+`
