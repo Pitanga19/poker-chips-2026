f022b57a1fd4c6949b4332f6530dd9055e28b2ed
PitangaRack <lcmaciel99@gmail.com>
2026-01-14 12:15:21 -0300

    Aplicar AuthProvider

--- native/App.tsx
+++ native/App.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { ThemeProvider } from './src/theme/ThemeProvider'
 import { GlobalStyle } from './src/theme/GlobalStyle'
+import { AuthProvider } from './src/features/auth/store'
 import AppNavigator from './src/navigation/AppNavigator'
 
 export default function App() {
   return (
     <ThemeProvider>
       <GlobalStyle>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <AuthProvider>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </AuthProvider>
       </GlobalStyle>
     </ThemeProvider>
   )
