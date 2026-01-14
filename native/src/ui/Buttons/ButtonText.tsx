deadd347bbbd60e8a122f0079ec27ee15d56942c
PitangaRack <lcmaciel99@gmail.com>
2026-01-14 15:39:32 -0300

    Modificar formato de texto de botones

--- native/src/ui/Buttons/ButtonText.tsx
+++ native/src/ui/Buttons/ButtonText.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native'
 
 export const ButtonText = styled.Text`
+  text-transform: uppercase;
   color: ${({ theme }) => theme.colors.buttonText};
-  font-size: ${({ theme }) => theme.typography.sizes.md}px;
+  font-size: ${({ theme }) => theme.typography.sizes.sm}px;
   font-weight: ${({ theme }) => theme.typography.weights.medium};
 `
