dbd59340658ee77eb5ad05a009db750aedffc80c
PitangaRack <lcmaciel99@gmail.com>
2026-01-14 15:38:58 -0300

    Modificar estructura flex del contenedor de submit

--- native/src/ui/Forms/FormSubmit.tsx
+++ native/src/ui/Forms/FormSubmit.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native'
 
 export const SubmitContainer = styled.View`
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.sm}px;
   margin-top: auto;
 `
