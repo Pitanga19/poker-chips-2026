f48afb19aebcb07da7f3192ad0d17c08fd433a9b
PitangaRack <lcmaciel99@gmail.com>
2026-01-14 11:00:55 -0300

    Crear y aplicar storage para token

--- native/src/api/client.ts
+++ native/src/api/client.ts
@@ -1,6 +1,17 @@
 import axios from 'axios'
+import { authStorage } from '../features/auth/storage/auth.storage'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8000',
-    timeout: 10000,
+  baseURL: 'http://localhost:8000',
+  timeout: 10000,
+})
+
+api.interceptors.request.use((config) => {
+  const token = authStorage.getToken()
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  
+  return config
 })
