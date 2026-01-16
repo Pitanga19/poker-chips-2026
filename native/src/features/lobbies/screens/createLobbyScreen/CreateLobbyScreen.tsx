a7dcc1de1a3eae565795c2486b400f5858211a18
Pitanga <lcmaciel99@gmail.com>
2026-01-15 22:49:47 -0300

    Conectar con api de Lobbies para crearlo

--- native/src/features/lobbies/screens/createLobbyScreen/CreateLobbyScreen.tsx
+++ native/src/features/lobbies/screens/createLobbyScreen/CreateLobbyScreen.tsx
@@ -1,45 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { LobbyCreateRequest, UserInfo } from '../../types'
 import { RootStackParamList } from '../../../../navigation/AppStack'
 import { ScreenLayout } from '../../../../layout/ScreenLayout'
 import { Form, InputContainer, SubmitContainer } from '../../../../ui/Forms'
 import { TextInput, InputLabel, InputError } from '../../../../ui/Inputs'
 import { PrimaryButton, ButtonText } from '../../../../ui/Buttons'
+import { createLobby } from '../../../../api/lobbies'
+import { useAuth } from '../../../auth/hooks/useAuth'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'CreateLobby'>
 
 export default function CreateLobbyScreen({ navigation }: Props) {
+  const { user } = useAuth()
+  const id = user?.id ?? 0
+  const username = user?.username ?? ''
+  const [tableSize, setTableSize] = useState<string>('')
+  const [bigBlindValue, setBigBlindValue] = useState<string>('')
+  const [initialStack, setInitialStack] = useState<string>('')
+  const [selfPosition, setSelfPosition] = useState<string>('')
+  
   const handleBack = () => navigation.goBack()
+  
+  const handleSubmit = () => {
+    const createLobbyRequest: LobbyCreateRequest = {
+      hoster_info: { id, username },
+      table_size: parseInt(tableSize),
+      big_blind_value: parseInt(bigBlindValue),
+      initial_stack: parseInt(initialStack),
+      self_position: parseInt(selfPosition),
+    }
+    
+    createLobby(createLobbyRequest)
+  }
   
   return (
     <ScreenLayout title={'Crear Juego'} showBack={true} onBack={handleBack}>
       <Form>
         <InputContainer>
-          <InputLabel>Tamaño de la mesa</InputLabel>
-          <TextInput></TextInput>
+          <InputLabel>Cantidad de asientos</InputLabel>
+          <TextInput
+            placeholder='2 a 10'
+            value={tableSize}
+            onChangeText={setTableSize}
+          />
           <InputError></InputError>
         </InputContainer>
         
         <InputContainer>
-          <InputLabel>Su posición en la mesa</InputLabel>
-          <TextInput></TextInput>
+          <InputLabel>Valor de la ciega grande</InputLabel>
+          <TextInput
+            placeholder='valor en fichas'
+            value={bigBlindValue}
+            onChangeText={setBigBlindValue}
+          />
           <InputError></InputError>
         </InputContainer>
         
         <InputContainer>
           <InputLabel>Stack inicial</InputLabel>
-          <TextInput></TextInput>
+          <TextInput
+            placeholder='fichas iniciales'
+            value={initialStack}
+            onChangeText={setInitialStack}
+          />
           <InputError></InputError>
         </InputContainer>
         
         <InputContainer>
-          <InputLabel>Valor ciega grande</InputLabel>
-          <TextInput></TextInput>
+          <InputLabel>Su posición en la mesa</InputLabel>
+          <TextInput
+            placeholder='número de asiento'
+            value={selfPosition}
+            onChangeText={setSelfPosition}
+          />
           <InputError></InputError>
         </InputContainer>
         
         <SubmitContainer>
-          <PrimaryButton>
+          <PrimaryButton onPress={handleSubmit}>
             <ButtonText>Crear Juego</ButtonText>
           </PrimaryButton>
         </SubmitContainer>
